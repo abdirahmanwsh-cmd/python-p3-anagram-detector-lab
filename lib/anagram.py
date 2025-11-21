f375b3c9879c030f4048276180f2de43fb4465ad
Manka.code <Abdirahman.wsh@gmail.com>
2025-11-21 02:43:49 -0500

    adds anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self._key = ''.join(sorted(word.lower()))
+
+    def match(self, candidates):
+        matches = []
+        for cand in candidates:
+            if cand.lower() == self.word.lower():
+                continue
+            if ''.join(sorted(cand.lower())) == self._key:
+                matches.append(cand)
+        return matches
+
